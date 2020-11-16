fe93c03b029ed8132e32e9db1e9e6d055dd1b47b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-16 13:14:35 -0500

    Add the test cases for the memoized function in test_fibonacci.

--- fibonaccicreator/tests/test_fibonacci.py
+++ fibonaccicreator/tests/test_fibonacci.py
@@ -46,3 +46,24 @@ def test_sixth_fibonacci_recursive():
     number = 6
     result = fibonacci.fibonacci_recursive(number)
     assert result == 13
+
+
+def test_zeroth_fibonacci_default_recursive_memo():
+    """Ensure that the request for the Fibonacci recursive function with memoization works."""
+    number = 0
+    result = fibonacci.fibonacci_memoized(number)
+    assert result == 1
+
+
+def test_oneth_fibonacci_default_next_recursive_memo():
+    """Ensure that the request for the Fibonacci recursive function with memoization works."""
+    number = 1
+    result = fibonacci.fibonacci_memoized(number)
+    assert result == 1
+
+
+def test_sixth_fibonacci_recursive_memo():
+    """Ensure that the request for the Fibonacci recursive with memoization function works."""
+    number = 6
+    result = fibonacci.fibonacci_memoized(number)
+    assert result == 13
