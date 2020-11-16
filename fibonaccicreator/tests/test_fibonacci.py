e47623c626d52b7b4aca196bb932699b09ea62b9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-16 13:09:10 -0500

    Add all of the test cases for the recursive Fibonacci function.

--- fibonaccicreator/tests/test_fibonacci.py
+++ fibonaccicreator/tests/test_fibonacci.py
@@ -6,22 +6,43 @@ import sympy
 from fibonaccicreator import fibonacci
 
 
-def test_zeroth_fibonacci_default():
+def test_zeroth_fibonacci_default_iterative():
     """Ensure that the request for the Fibonacci iterative function works."""
     number = 0
     result = fibonacci.fibonacci_iterative(number)
     assert result == 1
 
 
-def test_oneth_fibonacci_default_next():
+def test_oneth_fibonacci_default_next_iterative():
     """Ensure that the request for the Fibonacci iterative function works."""
     number = 1
     result = fibonacci.fibonacci_iterative(number)
     assert result == 1
 
 
-def test_sixth_fibonacci():
+def test_sixth_fibonacci_iterative():
     """Ensure that the request for the Fibonacci iterative function works."""
     number = 6
     result = fibonacci.fibonacci_iterative(number)
+    assert result == 13
+
+
+def test_zeroth_fibonacci_default_recursive():
+    """Ensure that the request for the Fibonacci recursive function works."""
+    number = 0
+    result = fibonacci.fibonacci_recursive(number)
+    assert result == 1
+
+
+def test_oneth_fibonacci_default_next_recursive():
+    """Ensure that the request for the Fibonacci recursive function works."""
+    number = 1
+    result = fibonacci.fibonacci_recursive(number)
+    assert result == 1
+
+
+def test_sixth_fibonacci_recursive():
+    """Ensure that the request for the Fibonacci recursive function works."""
+    number = 6
+    result = fibonacci.fibonacci_recursive(number)
     assert result == 13
