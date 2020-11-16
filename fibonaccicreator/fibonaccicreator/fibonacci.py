81544c10d3bc1e9697ae9df1f79d9d5190134197
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-16 13:15:06 -0500

    Add the fibonacci_memoized function to the fibonacci module.

--- fibonaccicreator/fibonaccicreator/fibonacci.py
+++ fibonaccicreator/fibonaccicreator/fibonacci.py
@@ -2,6 +2,10 @@
 
 from memory_profiler import profile
 
+# create an empty dictionary used for storage
+# of previously computed Fibonacci values
+fibonacci_storage = {}
+
 
 @profile
 def fibonacci_iterative(number: int) -> int:
@@ -27,3 +31,22 @@ def fibonacci_recursive(number: int) -> int:
         return 1
     # recursive case
     return fibonacci_recursive(number - 2) + fibonacci_recursive(number - 1)
+
+
+@profile
+def fibonacci_memoized(number: int) -> int:
+    """Compute the number-th Fibonacci number using recursion."""
+    # memoized case
+    if number in fibonacci_storage:
+        return fibonacci_storage[number]
+    # base case
+    elif number == 0 or number == 1:
+        return 1
+    # recursive case
+    else:
+        # perform the recursive computation
+        result = fibonacci_recursive(number - 2) + fibonacci_recursive(number - 1)
+        # store the value in the dictionary
+        fibonacci_storage[number] = result
+        # return the result
+        return result
